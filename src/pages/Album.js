e5321d3750ec36fd9fa4d889e80eec38dce636f6
Renan Storrer <storrerrenan@gmail.com>
2022-09-03 03:58:24 -0300

    Requisito 7

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import Header from '../components/Header';
+import React, { Component } from 'react';
 
-class Album extends React.Component {
+import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+
+import getMusics from '../services/musicsAPI';
+
+class Album extends Component {
+  state = {
+    albumCollection: {},
+    albumTracks: [],
+  };
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const albumData = await getMusics(id);
+    const albumCollection = albumData.filter((_, i) => i === 0)[0];
+    const albumTracks = albumData.filter((_, i) => i !== 0);
+    this.setState({ albumCollection, albumTracks });
+  }
+
   render() {
+    const { albumCollection, albumTracks } = this.state;
+    console.log(albumCollection);
+    console.log(albumTracks);
     return (
-      <div>
+      <div data-testid="page-album">
         <Header />
-        <p data-testid="page-album">ALBUM</p>
+        <h1 data-testid="artist-name">{albumCollection.artistName}</h1>
+        <h2 data-testid="album-name">{albumCollection.collectionName}</h2>
+        {albumTracks.map(({ trackName, previewUrl }) => (
+          <MusicCard
+            key={ previewUrl }
+            trackName={ trackName }
+            previewUrl={ previewUrl }
+          />
+        ))}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.string,
+}.isRequired;
 
 export default Album;
